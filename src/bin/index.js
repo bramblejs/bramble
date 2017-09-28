ddbfa05f7736b5125eb99672629b25d31ed2f55c
Trey Shugart <treshugart@gmail.com>
2017-09-28 11:19:31 +1000

    Add scripts and base api files.

---
+++ src/bin/index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const yargs = require('yargs');
+
+const { argv } = yargs;
+const cmd = argv._.join(path.delimiter) || 'build';
+
+if (fs.existsSync(path.join(__dirname, `${cmd}.js`))) {
+  require(`./${cmd}`)(argv);
+} else {
+  throw new Error(`Command not found: ${cmd}`);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-// This is the main entry point.
+const build = require('./build');
+const check = require('./check');
+
+module.exports = {
+  build,
+  check
+};
