a51673f4f758c7188d1f539fc0830e16412958b0
Trey Shugart <treshugart@gmail.com>
2017-09-28 12:00:27 +1000

    Generating size / date information now works.

--- src/bin/build.js
+++ src/bin/build.js
@@ -2,11 +2,35 @@ const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
 
+const exists = promisify(fs.exists);
+const glob = promisify(require('glob'));
 const mkdirp = promisify(require('mkdirp'));
+const readFile = promisify(fs.readFile);
 const rimraf = promisify(require('rimraf'));
+const stat = promisify(fs.stat);
+const writeFile = promisify(fs.writeFile);
 
-module.exports = async ({ outDir = 'bramble', srcFiles = 'src/**/*.js' } = {}) => {
-  const outDirRel = path.join(process.cwd(), outDir);
+module.exports = async (
+  { outDir = 'bramble', srcFiles = 'src/**/*.js' } = {}
+) => {
+  const schemaFilePath = path.join(outDir, 'schema.json');
+  const existingSchema = (await exists(schemaFilePath))
+    ? JSON.parse(await readFile(schemaFilePath))
+    : { data: [] };
+
   await rimraf(outDir);
   await mkdirp(outDir);
-}
+
+  const date = new Date().toGMTString();
+  const size = await (await glob(srcFiles)).reduce(
+    async (prev, curr) => (await prev) + (await stat(curr)).size,
+    Promise.resolve(0)
+  );
+
+  existingSchema.data = existingSchema.data.concat({
+    date,
+    size
+  });
+
+  writeFile(schemaFilePath, JSON.stringify(existingSchema));
+};
