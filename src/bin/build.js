7b379c5733a3fc08f5ef5d0c7518129ccd0fbb51
Trey Shugart <treshugart@gmail.com>
2017-09-29 14:54:20 +1000

    Reuse more code.

--- src/bin/build.js
+++ src/bin/build.js
@@ -5,14 +5,13 @@ const { promisify } = require('util');
 const webpack = promisify(require('webpack'));
 
 module.exports = async args => {
-  await fs.copy(
-    path.join(__dirname, '..', '..', 'app', 'index.html'),
-    path.join(args.outDir, 'index.html')
-  );
-  await fs.copy(
-    path.join(__dirname, '..', '..', 'app', 'index.js'),
-    path.join(args.outDir, 'index.js')
-  );
+  const copy = async file =>
+    await fs.copy(
+      path.join(__dirname, '..', '..', 'app', file),
+      path.join(args.outDir, file)
+    );
+  await copy('index.html');
+  await copy('index.js');
   await webpack({
     entry: `${args.outDir}/index.js`,
     output: {
